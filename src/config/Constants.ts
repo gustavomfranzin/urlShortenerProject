bb4a43df2976f357789f6f4e9bd504b5054b7f69
gustavomfranzin <gusfranzin@gmail.com>
2021-11-23 03:55:33 -0300

    remove credentials db

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:'mongodb+srv://adminDio:FtRnGDPpwXRmBcNs@url-shortener-dio.hxh0b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+	MONGO_CONNECTION:'mongodb+srv://<userdb>:<password>@url-shortener-dio.hxh0b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
